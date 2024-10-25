04acae51e027c4996c839a0bf24176226a335323
Summit1122 <155012691+Summit1122@users.noreply.github.com>
2024-10-25 16:53:57 -0500

    Post

--- program_3.py
+++ program_3.py
@@ -6,3 +6,70 @@
 # The program should count of the number of correct and incorrect responses.  
 # (You could alternatively use another country and provinces, 
 # or countries of the world and capitals).
+import random
+state_names= capital_dic={           #dictionary of states and their capitals pull from the internet
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona':'Phoenix',
+    'Arkansas':'Little Rock',
+    'California': 'Sacramento',
+    'Colorado':'Denver',
+    'Connecticut':'Hartford',
+    'Delaware':'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Monies',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'St. Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhoda Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakota': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne' }
+
+def states_dictionary():
+    random_state = random.choice(list(state_names.items()))
+    answer = input(f"What is the capital of {random_state[0]}?: ")
+    if answer  in random_state:
+        print("That is correct! great job!")
+    else:
+        print("Sorry that is incorrect.")
+
+
+
+
+
+
+states_dictionary()
--- program_5.py
+++ program_5.py
@@ -1,5 +1,33 @@
 # Program #5: Course Info
+# Luis Amador
+# 10/22/24
 # Write a program that has the user input a bunch of course ID and course name pairs.  
 # For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
 # Then ask the user for a subject (like "COS"). 
 # Finally, the program will display the ID and name of all the courses having that subject.
+
+
+def new_course_adder():
+    loops = 'true'
+    course_info = ''
+    while loops == 'true':
+        course_id = input("What is the course ID? (ex: COS 2005): ")
+        course_name = input("What is the name of the course?: ")
+        entry = course_id + course_name
+        course_info = course_info + entry
+        ques = input("Would you like to add another course? y or n: ")
+        if ques == 'y':
+            loops = 'true'
+        else:
+            loops = 'false'
+
+
+def main():
+    new_course_adder()
+    list = ''
+    subject = input("What subject list would you like to see? (ex: COS): ")
+    for string in course_info:
+        if subject in string:
+            list = list + string
+
+main()
